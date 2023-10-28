5d498bc735776d2e2504c45f49748666d65de829
dina <dina@DESKTOP-5J66LQ6>
2023-10-28 16:34:36 +0200

    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

--- GIT/src/udemy/Hello.java
+++ GIT/src/udemy/Hello.java
@@ -3,7 +3,7 @@ package udemy;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Welcom to GIT courses!!!");
+		System.out.println("Welcom to GIT courses!!!!");
 	}
 
 }
